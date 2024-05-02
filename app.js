b4c214286aa166e57d9f0ff0a020593f41b5c2d1
Angie <liya0706s@gmail.com>
2024-05-02 12:14:49 +0800

    feat:Re-bulilding the App with Vue

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-
+Vue.createApp({
+    data(){
+        return {
+            goals:[],
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue= ''; //清除輸入框的值
+        }
+    }
+}).mount('#app');
 
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
