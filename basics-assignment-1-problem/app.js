3059721c03da1396f1eabe610bec5add24bd1fc5
Angie <liya0706s@gmail.com>
2024-05-03 16:40:59 +0800

    feat: practice Vue Binding output data

--- basics-assignment-1-problem/app.js
+++ basics-assignment-1-problem/app.js
@@ -2,12 +2,13 @@ const app = Vue.createApp({
     data(){
         return{
             yourName: 'Angie',
-            yourAge: 18
+            yourAge: 18,
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/53/Snoopy_Peanuts.png'
         }
     },
-    computed:{
-        ageInFiveYears(){
-            return this.yourAge + 5;
+    methods:{
+        calculateAge(){
+            return this.yourAge + 5
         },
         randomFavNum(){
             return Math.random();
