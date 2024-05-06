d97757212cbd6d41b78885b9ba699b16899f713c
Angie <liya0706s@gmail.com>
2024-05-06 17:16:28 +0800

    feat:2 inputs 2 v-models

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,34 +4,62 @@ const app = Vue.createApp({
     return {
       counter: 10,
       name: '',
-      confirmName: ''
+      // confirmName: ''
+      lastName:'',
+      fullname: ''
     };
   },
-  methods:{
-    confirmInput(){
-      this.confirmName=this.name;
+  watch: {
+    name(value) {
+      if (value === '') {
+        this.fullname = '';
+      } else {
+        this.fullname = value + ' ' + this.lastName;
+      }
     },
-    submitForm(){
+    lastName(value) {
+      if (value === '') {
+        this.fullname = '';
+      } else {
+        this.fullname = this.name + ' ' + value;
+      }
+    },
+  },
+  computed: {
+    // fullname(){
+    //   console.log('Running again...');
+    //   if(this.name === ''){
+    //     return '';
+    //   }
+    //   return this.name + ' ' + 'Lee';
+    // }
+  },
+  methods: {
+    confirmInput() {
+      this.confirmName = this.name;
+    },
+    submitForm() {
       // event.preventDefault();
       alert('Submitted!');
     },
-    outputFullname(){
-      if(this.name === ''){
+    outputFullname() {
+      console.log('Running again...');
+      if (this.name === '') {
         return '';
       }
       return this.name + ' ' + 'Lee';
     },
-    setName(event){
-      this.name=event.target.value;
+    setName(event) {
+      this.name = event.target.value;
     },
-    add(num){
-      this.counter= this.counter + num;
+    add(num) {
+      this.counter = this.counter + num;
     },
-    reduce(num){
-      this.counter= this.counter - num;
+    reduce(num) {
+      this.counter = this.counter - num;
     },
-    resetInput(){
-      this.name= '';
+    resetInput() {
+      this.name = '';
     }
   }
 });
